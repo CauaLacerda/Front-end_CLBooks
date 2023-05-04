ed14e6f640233df73d8dce2938ac4bda5e37d97c
Cauã Alves Lacerda <115109611+CauaLacerda@users.noreply.github.com>
2023-05-04 15:54:41 -0300

    Update code.js

---
+++ js/code.js
@@ -0,0 +1,5 @@
+var btFechar = document.querySelector('.aviso-cookies botao-fechar-politica')
+var aviso = document.getElementsByClassName('aviso')
+
+btFechar.addEventListener('click', () => {
+    aviso.style.display = 'none'})
