a60a94201299e0b4ec4e89dfd4c0ca3499a90f91
Cauã Alves Lacerda <115109611+CauaLacerda@users.noreply.github.com>
2023-05-18 15:03:25 -0300

    update

--- js/code.js
+++ js/code.js
@@ -4,3 +4,23 @@ var div = document.querySelector('.aviso-cookies');
 btn.addEventListener('click', function(){
     div.style.display = 'none';
 });
+
+
+
+
+// mostra a seta quando rolagem for maior que x
+const rolagem = ()=>{
+    const tela = document.querySelector('html')
+    const seta = document.querySelector('.seta-sobe')
+    //console.log(tela.scrollTop)
+    if(tela.scrollTop > 200){
+        seta.style.display = 'block'
+
+    }else{
+        seta.style.display = 'none'
+    }
+}
+//window.addWventListener('scroll', rolagem)
+window.onscroll = ()=> rolagem()
+
+
